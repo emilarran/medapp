995c46aa9de61aa47c9af825e1582c7131342cd7
Emil Arran Baleva <arranbaleva@gmail.com>
2018-12-03 04:42:21 +0800

    Create functionalities for medicines crud

--- src/app/medicines/medicines.component.ts
+++ src/app/medicines/medicines.component.ts
@@ -20,4 +20,46 @@ export class MedicinesComponent implements OnInit {
     this.getMedicines();
   }
 
+  medicine: Medicine = {
+    id: null,
+    name: null,
+    price: null,
+    manufacturer: null
+  };
+
+  addMedicine(): void {
+    this.medicine.id = this.medicines[this.medicines.length-1].id + 1;
+    this.medService.addMedicine(this.medicine as Medicine).subscribe(medicine => {
+      this.medicines.push(medicine);
+    });
+    this.resetMedicine();
+  }
+
+  resetMedicine(): void {
+    this.medicine.id = null;
+    this.medicine.name = null;
+    this.medicine.price = null;
+    this.medicine.manufacturer = null;
+  }
+
+  deleteMedicine(index: number): void {
+    let toBeDeleted = this.medicines[index];
+    this.medicines = this.medicines.filter(h => h !== toBeDeleted);
+    this.medService.deleteMedicine(toBeDeleted).subscribe();
+  }
+
+  selectedMedicine: Medicine = {
+    id: null,
+    name: null,
+    price: null,
+    manufacturer: null
+  };
+
+  selectMedicine(index: number): void {
+    this.selectedMedicine = this.medicines[index];
+  }
+
+  editMedicine(): void {
+    this.medService.updateMedicine(this.selectedMedicine).subscribe();
+  }
 }
