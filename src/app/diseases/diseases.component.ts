18388adf6c8757298c1ab8799bee76ea8e5dfea9
Emil Arran Baleva <arranbaleva@gmail.com>
2018-12-03 02:24:19 +0800

    Add create new disease functionality

--- src/app/diseases/diseases.component.ts
+++ src/app/diseases/diseases.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MedService } from '../med.service';
 import { Disease } from '../disease';
+import { Medicine } from '../medicine';
 
 @Component({
   selector: 'app-diseases',
@@ -14,10 +15,39 @@ export class DiseasesComponent implements OnInit {
     this.medService.getDiseases().subscribe(diseases => this.diseases = diseases);
   }
 
+  medicines: Medicine[];
+
+  getMedicines(): void {
+    this.medService.getMedicines().subscribe(medicines => this.medicines = medicines);
+  }
+
   constructor(private medService: MedService) { }
 
   ngOnInit() {
       this.getDiseases();
+      this.getMedicines();
   }
 
+  disease: Disease = {
+    id: null,
+    name: null,
+    medicines: []
+  };
+
+  addDisease(id: number): void {
+    this.disease.id = this.diseases[this.diseases.length-1].id + 1;
+    this.disease.medicines.pop();
+    this.disease.medicines.push(this.medicines[id]);
+    console.log(this.disease);
+    this.medService.addDisease(this.disease as Disease).subscribe(disease => {
+      this.diseases.push(disease);
+    });
+    this.resetDisease();
+  }
+
+  resetDisease(): void {
+    this.disease.id = null;
+    this.disease.name = null;
+    this.disease.medicines = [];
+  }
 }
